a477b5cf600f72fd123a16fd0f00d7d4f2cad0de
Abhinav Naman <93396109+AbhinavNaman@users.noreply.github.com>
2023-10-21 22:35:24 +0530

    Update posts.js

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -8,7 +8,7 @@ import { getPost,
 
 
 router.route("/post").post(getPost)
-router.route("/posts").post(getPosts)
+router.route("/posts").get(getPosts)
 router.route("/favPosts").post(getFavPosts)
 
 export default router;
